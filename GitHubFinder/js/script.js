1b5a165903ac9d1c07e471be77a9e2532f65b32b
kedarSedai <kedar.sedai05@gmail.com>
2020-03-23 12:52:40 +0545

    Changes in view

--- GitHubFinder/js/script.js
+++ GitHubFinder/js/script.js
@@ -13,21 +13,18 @@ $(document).ready(function(){
     }).done(function(user){
         $('#profile').html(`
         
-        <div class="panel panel-default">
-         <div class="panel-heading">
-          <h3 class="panel-title">${user.name}</h3>
-        </div>
-          <div class="panel-body">
+          <h3 class="title">${user.name}</h3>
+          <div class="body">
             <div class="row">
-                <div class="col-md-3">
-                    <img  class="thumbnail" src="${user.avatar_url}">
+                <div class="col-md-4">
+                    <img  class="photo" src="${user.avatar_url}">
                     <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
                     <ul class="list-group">
                     <li class="list-group-item">Company: ${user.company}</li>
                     <li class="list-group-item">${user.blog}</li>
                 </ul>
                 </div>
-                <div class="col-md-9">
+                <div class="col-md-8">
                 <button class="btn btn-success">Public Repos: ${user.public_repos}</button>
                 <button class="btn btn-primary">Public Gists: ${user.public_gists}</button>
                 <button class="btn btn-info">Followers: ${user.followers}</button>
